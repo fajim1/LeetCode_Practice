6a1327ecf14ebb985ec88220846ef62fc53f19aa
Fahim Ishrak <fahim.ishrak1@gmail.com>
2022-07-28 12:57:03 -0400

    Time: 545 ms (5.05%), Space: 15.6 MB (22.24%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -4,26 +4,21 @@ class Solution:
         left = 0
         right = len(nums)-1
         mid = (left+right)//2
-        
-        countl = 0
-        countr = 0
-        while countl<=2 and countr <=2:
+
+        while left<= right:
             
             if nums[mid] < target:
                 left = mid+1
             
             elif nums[mid] > target:
-                right = mid 
+                right = mid - 1
                 
             else:
                 return mid
             
             mid = (left+right)//2
             
-            if mid==left:
-                countl+=1
-            elif mid == right:
-                countl+=1
+
             
         return -1
                 
