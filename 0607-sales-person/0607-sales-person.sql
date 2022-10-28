86369b66c5d5c95e115da2c841789579e180ed55
Fahim Ishrak <fahim.ishrak1@gmail.com>
2022-10-28 11:27:20 -0400

    Time: 1204 ms (78.60%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -4,9 +4,9 @@ SELECT name
 FROM SalesPerson
 WHERE sales_id not in (
 
-SELECT Orders.sales_id
-FROM Orders 
-INNER JOIN Company 
-ON Orders.com_id = Company.com_id
-WHERE Company.name = "RED"
+    SELECT Orders.sales_id
+    FROM Orders 
+    INNER JOIN Company 
+    ON Orders.com_id = Company.com_id
+    WHERE Company.name = "RED"
 )
